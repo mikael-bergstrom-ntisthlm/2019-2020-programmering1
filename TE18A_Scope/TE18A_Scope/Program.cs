3ac02440747241af7d9065c7122da89773fe57a0
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2019-10-14 10:24:23 +0200

    Scope med TE18A

---
+++ TE18A_Scope/TE18A_Scope/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TE18A_Scope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 9;
+
+            if (4 < 5)
+            {
+                y = 9;
+                if (true)
+                {
+                    y = 10;
+                }
+            }
+
+            if (2 == 2)
+            {
+                y = 99;
+            }
+
+            Console.WriteLine(y);
+
+
+
+        }
+    }
+}
