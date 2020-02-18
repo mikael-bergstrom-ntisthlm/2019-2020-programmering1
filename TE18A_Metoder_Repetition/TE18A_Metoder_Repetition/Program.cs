7c5ea15ba22dbfb71a2f4bff7143e97d95d500ff
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2020-02-18 13:43:59 +0100

    Repetition av metoder med 18A

---
+++ TE18A_Metoder_Repetition/TE18A_Metoder_Repetition/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TE18A_Metoder_Repetition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = 5;
+
+            int min = 3;
+            int max = 10;
+
+            int u = 0;
+            if (int.TryParse(Console.ReadLine(), out u))
+            {
+                u = LimitRange(u, min, max);
+            }
+            EnMetod();
+        }
+
+        static int LimitRange(int value, int min, int max)
+        {
+            if (value < min)
+            {
+                return min;
+            }
+            if (value > max)
+            {
+                return max;
+            }
+
+            return value;
+        }
+
+        
+
+        static void EnMetod()
+        {
+            Console.WriteLine("Do Something");
+
+            Console.WriteLine(t);
+
+            while (1 == 1)
+            {
+                Console.WriteLine("FOREVER");
+            }
+
+        }
+
+
+
+    }
+}
